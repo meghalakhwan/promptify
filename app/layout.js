e3064b9ba213d369c828db9b9ebc55cad6e720db
meghalakhwan <meghalakhwan9@gmail.com>
2024-06-21 01:37:28 +0530

    Add Navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import "@styles/globals.css";
 
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptify",
@@ -14,7 +16,9 @@ const RootLayout = ({children}) => {
           <div className="gradient" />
         </div>
 
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}</main>
       </body>
     </html>
   );
---
+++ components/Provider.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+const Provider = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Provider
